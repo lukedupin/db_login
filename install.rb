30e184f0173d26ae6a3097ae6007e47e62cd48ae
Rhino <orbital.sfear@gmail.com>
2009-01-13 17:28:02 -0800

    Miss named the install directory

--- install.rb
+++ install.rb
@@ -2,5 +2,5 @@
 require 'ftools'
 
 File.copy("#{File.dirname(__FILE__)}/lib/db_login",
-            "#{File.dirname(__FILE__)}/../../../scripts/db_login" )
+            "#{File.dirname(__FILE__)}/../../../script/db_login" )
 
