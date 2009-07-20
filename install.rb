45ef13d8989656b854b110a81304f2739014502b
Luke Dupin <ldupin@CRF450r.(none)>
2009-07-19 23:09:11 -0700

    Added the db_dump script to the install script

--- install.rb
+++ install.rb
@@ -3,4 +3,6 @@ require 'ftools'
 
 File.copy("#{File.dirname(__FILE__)}/lib/db_login",
             "#{File.dirname(__FILE__)}/../../../script/db_login" )
+File.copy("#{File.dirname(__FILE__)}/lib/db_dump",
+            "#{File.dirname(__FILE__)}/../../../script/db_dump" )
 
